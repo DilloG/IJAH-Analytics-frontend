2477436b0c5f3828693a4d7a97acf5f204c5945a
DilloG <dilloginoga@gmail.com>
2020-04-18 13:06:54 +0700

    update #29
    contact us

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, filter } from 'rxjs/operators';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  name:any;
+  email:any;
+  affiliation:any;
+  subject:any;
+  message:any;
+
+  showFailed:boolean = false;
+  showSuccess:boolean = false;
+  closeError(){
+    this.showFailed = false;
+    this.showload = false;
+  }
+
+  fileUpload:any;
+  closeSuccess(){
+    this.showSuccess = false;
+    this.showload = false;
+    this.name="",
+    this.email="",
+    this.affiliation="",
+    this.subject="",
+    this.message=""
+  }
+
+  getResult() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "X-Requested-With": "XMLHttpRequest",
+        "Content-type": "application/json"
+      })
+    };
+
+    const fd = {
+      name: this.name,
+      email: this.email,
+      affiliation: this.affiliation,
+      subject: this.subject,
+      message: this.message
+    }
+
+    const sendMsgJson = JSON.stringify(fd);
+    console.log(sendMsgJson);
+    this.http.post<any>("http://admin.vidner.engineer/feedback", sendMsgJson, httpOptions).toPromise().then(data => {
+      this.result = data;
+      if (this.result) {
+        console.log(this.result);
+        this.showSuccess = true;
+      }
+    }).catch(err => {
+      console.log(err.message);
+      this.errMsg = err.message;
+      this.showFailed = true;
+    });
+
   }
 
 }
